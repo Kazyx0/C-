f7495c302f4c0421f0361ce373fafe6a609105dc
Kazyx0 <krmyilmaz9@gmail.com>
2024-08-05 13:22:52 +0300

    Add files via upload

--- toplamayap/obj/Debug/net8.0/Toplamayap.AssemblyInfo.cs
+++ toplamayap/obj/Debug/net8.0/Toplamayap.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Toplamayap")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("toplamayap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Toplamayap")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Toplamayap")]
+[assembly: System.Reflection.AssemblyProductAttribute("toplamayap")]
+[assembly: System.Reflection.AssemblyTitleAttribute("toplamayap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
